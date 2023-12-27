7c001d0bbf589704a044dfd7bc2d6b14ef1ff8b2
shaysnutss <akshayan.2020@scis.smu.edu.sg>
2023-12-28 00:05:34 +0800

    made first page responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,8 +7,11 @@ export default function Home() {
     return (
     <div className="App">
       <Spline className="animation" scene="https://prod.spline.design/9h4NbPM9-WWQH9VJ/scene.splinecode" />
-      <h1 className="title" id = "title1" >Meet your</h1>
-      <h1 className="title" id = "title2">Next dev</h1>
+      <div className="flexContainer">
+        <h1 className="title" id = "title1" >Meet your</h1>
+        <h1 className="title" id = "title2">Next dev</h1>
+      </div>
+
     </div>
     )
 }
