9b029769590ebd555cee25bfb27ebe22a5ba3aab
shaysnutss <akshayan.2020@scis.smu.edu.sg>
2023-12-28 01:13:28 +0800

    working copy before flex

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import './Skills.css';
+import Spline from '@splinetool/react-spline';
 
 export default function Skills() {
     return (
     <div className ="background">
-      <h1>
-        <center>
-        <p>
-          This is SKILLS.
-        </p>
-      </center>
-      </h1>
+      <Spline className = "flexChild" scene="https://prod.spline.design/LQ6f2WBGgg00r6AV/scene.splinecode" />
+      <div className = "flexChild">
+        hi
+      </div>
     </div>
     )
 }
