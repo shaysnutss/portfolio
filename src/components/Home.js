b183e8bc8754d0d0867a1a9b504032621d1728c9
shaysnutss <akshayan.2020@scis.smu.edu.sg>
2024-01-03 20:21:17 +0800

    all changes but bg video

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,15 @@ import React from 'react';
 import logo from '../logo.svg';
 import Spline from '@splinetool/react-spline';
 import './Home.css';
+import bg from '../assets/home/bg.mp4';
 
 export default function Home() {
     return (
     <div className="App">
-      <Spline className="animation" scene="https://prod.spline.design/9h4NbPM9-WWQH9VJ/scene.splinecode" />
+      <video autoPlay loop muted id="video">
+        <source src={bg} type="video/mp4" />
+      </video>
+      {/* <Spline className="animation" scene="https://prod.spline.design/9h4NbPM9-WWQH9VJ/scene.splinecode" /> */}
       <div className="flexContainer">
         <h1 className="title" id = "title1" >Meet your</h1>
         <h1 className="title" id = "title2">Next dev</h1>
